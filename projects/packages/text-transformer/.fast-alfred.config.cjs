2fb88606d5e39511a034393d97ad9e7ebf06b96c
Aviv Ben Shahar <avivbens87@gmail.com>
2025-03-29 04:07:14 +0300

    feat: introduce `text-transformer` workflow 🥷

---
+++ projects/packages/text-transformer/.fast-alfred.config.cjs
@@ -0,0 +1,56 @@
+const { resolve } = require('node:path');
+const { author, description, homepage } = require('./package.json');
+const { env } = require('node:process');
+
+const README = `
+#### Thank you for using Text Transformer! ✨
+
+This workflow has been created using Fast Alfred, a user-friendly workflow builder that simplifies the process of creating Alfred workflows.
+
+## Features
+
+- Grammar: Correct grammar mistakes
+- Beautify: Enhance the text for better readability and engagement
+- Spell Check: Identify spelling errors without grammar checking
+- Translate: Translate text into any language
+- Tones: Change the tone of the text be giving input
+- Summarize: Summarize the text to be more concise
+- Explain: Explain the text in a more understandable way
+
+## Usage
+
+Use your customized Alfred keyword to activate each command.
+
+### API key
+
+You can generate an OpenAI key via the following link:
+
+https://platform.openai.com/settings/organization/api-keys
+
+### Translate
+
+Enter the language code (e.g., "en" for English) followed by the text to be translated, like "en Hello, how are you?"
+
+If the language code is missing, the default language will be English.
+
+To view the workflow codebase, click here:
+${homepage}
+`.trim();
+
+/**
+ * @type {import('fast-alfred').FastAlfredConfig}
+ */
+module.exports = {
+  bundlerOptions: {
+    rootAssets: ['List Filter Images'],
+    targetDir: resolve(__dirname, '../../../', 'dist', env.NX_TASK_TARGET_PROJECT, 'esbuild'),
+  },
+  workflowMetadata: {
+    name: 'Text Transformer',
+    createdby: author.name,
+    webaddress: homepage,
+    description,
+    readme: README,
+  },
+  tabSize: 2,
+};
---
+++ projects/packages/text-transformer/src/main/tones.ts
@@ -0,0 +1,61 @@
+import type { AlfredListItem } from 'fast-alfred';
+import { FastAlfred } from 'fast-alfred';
+import { setTimeout } from 'node:timers/promises';
+import { DEFAULT_DEBOUNCE_TIME } from '../common/defaults.constants';
+import { TONE_SYSTEM_PROMPT } from '../common/prompts/tone.prompt';
+import { Variables } from '../common/variables.enum';
+import { AvailableModels } from '../models/available-models.enum';
+import { AvailableTone } from '../models/tones.enum';
+import { callModel } from '../services/llm.service';
+
+(async () => {
+  const alfredClient = new FastAlfred();
+
+  try {
+    const denounceTime = alfredClient.env.getEnv(Variables.DEBOUNCE_TIME, {
+      defaultValue: DEFAULT_DEBOUNCE_TIME,
+      parser: Number,
+    });
+    const token: string | undefined = alfredClient.env.getEnv(Variables.LLM_TOKEN);
+    const model: AvailableModels | undefined = alfredClient.env.getEnv(Variables.SELECTED_MODEL);
+
+    if (!token || !model) {
+      throw new Error('Token or model is not defined!');
+    }
+
+    if (!alfredClient.input) {
+      throw new Error('Input is required');
+    }
+
+    const [tone, ...rest] = alfredClient.input.split(' ');
+    const query = rest.join(' ').trim();
+
+    if (!query) {
+      return;
+    }
+
+    if (!Object.values(AvailableTone).includes(tone as AvailableTone)) {
+      throw new Error(`Not allowed tone! Please use one of: ${Object.values(AvailableTone).join(', ')}`);
+    }
+
+    /**
+     * Debounce time to wait for the user to finish typing
+     */
+    await setTimeout(denounceTime);
+
+    const system = await TONE_SYSTEM_PROMPT(tone as AvailableTone).format({});
+    const res = await callModel(token, model, { system, user: query });
+
+    const items: AlfredListItem[] = [
+      {
+        title: res,
+        subtitle: 'Tones',
+        arg: res,
+      },
+    ];
+
+    alfredClient.output({ items });
+  } catch (error) {
+    alfredClient.error(error);
+  }
+})();

dc6241fc40b11d9690f7fe3ced98a53e055aa115
Aviv Ben Shahar <avivbens87@gmail.com>
2025-03-29 17:58:52 +0300

    feat: introduce `advance-fs-search` workflow 🥷

---
+++ projects/packages/advance-fs-search/.fast-alfred.config.cjs
@@ -0,0 +1,50 @@
+const { resolve } = require('node:path');
+const { author, description, homepage } = require('./package.json');
+const { env } = require('node:process');
+
+const README = `
+#### Thank you for using Advance Fs Search! ✨
+
+This workflow has been created using Fast Alfred, a user-friendly workflow builder that simplifies the process of creating Alfred workflows.
+
+## Description
+
+Search over your file system with [Alfred](https://www.alfredapp.com/).
+
+## Features 🥷
+
+- Support VSCode as an editor
+- Supported terminals:
+  - iTerm2
+  - Warp
+  - Terminal.app
+
+## Usage
+
+1. Configure your search settings over the workflow settings.
+1. Use the keywords you've configured to open the results via VSCode / your selected Terminal ✨
+
+
+To view the workflow codebase, click here:
+${homepage}
+`.trim();
+
+/**
+ * @type {import('fast-alfred').FastAlfredConfig}
+ */
+module.exports = {
+  bundlerOptions: {
+    outputFormat: 'esm',
+    esmHelpers: true,
+    targetDir: resolve(__dirname, '../../../', 'dist', env.NX_TASK_TARGET_PROJECT, 'esbuild'),
+  },
+  workflowMetadata: {
+    name: 'Search File System',
+    category: 'Tools',
+    createdby: author.name,
+    webaddress: homepage,
+    description,
+    readme: README,
+  },
+  tabSize: 2,
+};
---
+++ projects/packages/advance-fs-search/README.md
@@ -0,0 +1,24 @@
+<div align="center">
+
+# Alfred Advance Fs Search
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/kcao7snkgx)
+
+</div>
+
+## Description
+
+Search over your file system with [Alfred](https://www.alfredapp.com/).
+
+## Features 🥷
+
+- Support VSCode as an editor
+- Supported terminals:
+  - iTerm2
+  - Warp
+  - Terminal.app
+
+## Usage
+
+1. Configure your search settings over the workflow settings.
+1. Use the keywords you've configured to open the results via VSCode / your selected Terminal ✨

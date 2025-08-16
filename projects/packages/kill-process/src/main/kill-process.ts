d7c274e8ba4bb3921affc1c4fa3107c86a0e2725
Aviv Ben Shahar <avivbens87@gmail.com>
2025-08-16 16:43:10 +0300

    chore: error handling for kill-process

--- projects/packages/kill-process/src/main/kill-process.ts
+++ projects/packages/kill-process/src/main/kill-process.ts
@@ -5,8 +5,15 @@ import { CallbackPayload } from '../models/callback-payload.model';
 (async () => {
   const alfredClient = new FastAlfred();
 
-  const { pid, shouldForceKill }: CallbackPayload = JSON.parse(alfredClient.input);
-  const args = [shouldForceKill ? '-9' : '', String(pid)].filter(Boolean);
+  try {
+    const { pid, shouldForceKill }: CallbackPayload = JSON.parse(alfredClient.input);
+    const args = [shouldForceKill ? '-9' : '', String(pid)].filter(Boolean);
 
-  await $({ args })`kill`;
+    const { stderr } = await $({ args, nothrow: true })`kill`;
+    if (stderr) {
+      throw new Error(stderr);
+    }
+  } catch (error) {
+    alfredClient.error(error);
+  }
 })();
